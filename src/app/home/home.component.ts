c6e7da1f5ca46de4baedb98943ccde19c9a2920e
supersakana <zacwilliamson@icloud.com>
2023-09-05 10:11:07 -0600

    added json server, data is loading through http requests

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -45,8 +45,10 @@ export class HomeComponent {
     );
   }
 
-  constructor(){
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 }
