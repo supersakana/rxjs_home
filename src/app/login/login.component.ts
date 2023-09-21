d3d9c38ebddaff9eb6096ce1b3be452c8b946939
supersakana <zacwilliamson@icloud.com>
2023-09-21 11:57:56 -0600

    styled and created basic forms for registering and login in

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule,
+    RouterModule
+  ],
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  submitApplication() {
+    console.log(
+      this.loginForm.value.email ?? '',
+      this.loginForm.value.password ?? ''
+    );
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule,
+    RouterModule
+  ],
 })
 export class RegisterComponent {
 
+  registerForm = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submitApplication() {
+    console.log(
+      this.registerForm.value.username ?? '',
+      this.registerForm.value.email ?? '',
+      this.registerForm.value.password ?? ''
+    );
+  }
 }
